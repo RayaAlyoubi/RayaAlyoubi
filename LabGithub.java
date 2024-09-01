85d081dda1560f11821042995a3671205721ca69
RayaAlyoubi <ralyoubi0084@stu.kau.edu.sa>
2024-09-01 20:39:42 +0300

    Add files via upload

---
+++ LabGithub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labgithub;
+
+/**
+ *
+ * @author Raya
+ */
+public class LabGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Raya M Alyoubi");
+    }
+    
+}
